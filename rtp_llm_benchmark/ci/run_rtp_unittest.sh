6f0be94dbefd086f8af5789c9aa9b46671b760e6
lcong-amd <lcong@amd.com>
2026-07-28 09:58:22 +0800

    clean rerun to verify iommu on fresh logs

--- rtp_llm_benchmark/ci/run_rtp_unittest.sh
+++ rtp_llm_benchmark/ci/run_rtp_unittest.sh
@@ -46,3 +46,4 @@ install_and_test_ut
 exit $EXIT_CODE
 # trigger Thu Jul 23 04:54:00 PM CST 2026
 # trigger CI after iommu=pt Mon Jul 27 06:59:37 PM CST 2026
+# clean rerun Tue Jul 28 09:58:22 AM CST 2026
